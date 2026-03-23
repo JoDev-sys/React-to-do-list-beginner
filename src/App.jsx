b1f8940adf3b410ea625f1d0d5b4dd9bc4566ed8
Joselito Penas <jopenas23@gmail.com>
2026-03-23 17:33:50 +0800

    Added and used useRef hook to access and reference value from DOM element input

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { FirstComponent } from './components/FirstComponent';
 
 const App = () => {
 
-  const [click, setClick] = useState(0);
-
-  const btnClick = () => {
-    setClick(click+1);
-  }
-
-  const subBtnClick = () => {
-    setClick(click-1);
-  }
-
+  // useref state hook to make reference to the input element
+  // accessing the value of the input element using useref and adding the value to the data array using the spread operator to create a new array with the existing data and the new value from the input element
+  const [data, setData] = useState([]);
+  const inputRef = useRef(null); 
   return (
     <div>
-      <button onClick={btnClick}>click + 1</button>
-      <br />
-      <button onClick={subBtnClick} style={{ marginTop: '20px' }}>click - 1</button>
-      <FirstComponent data={click} fn={setClick} />
+      <input ref={inputRef} type="text" placeholder='Enter something' />
+      <button onClick={() => {setData([...data, inputRef.current.value]);}}>Submit</button>
+      {data.map((item, index) => {
+        return <h2 key={index}>{item}</h2>
+        })}
     </div>
   )
  }
